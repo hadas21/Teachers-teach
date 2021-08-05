47bc4663dbc5a23eb426f099a8320293a4b67cc4
hadas21 <hadas.axel@gmail.com>
2021-08-05 15:27:10 -0500

    add routes: update, delete

--- app/routes/lesson_routes.js
+++ app/routes/lesson_routes.js
@@ -2,13 +2,15 @@
 const express = require('express')
 const router = express.Router()
 
-
 //passport
 const passport = require('passport')
 const requireToken = passport.authenticate('bearer', { session: false })
 
 //costum errors
-const { handle404 } = require('../../lib/custom_errors')
+const { handle404, requireOwnership } = require('../../lib/custom_errors')
+
+//middleware 
+const removeBlanks = require('../../lib/remove_blank_fields')
 
 //model
 const Lesson = require('../models/lesson')
@@ -36,6 +38,44 @@ router.get('/lessons', requireToken, (req, res, next) => {
         .then(lessons => {
             res.status(200).json({ lessons })
         })
+        .catch(next)
+})
+
+//update
+router.patch('/lessons/:id', requireToken, removeBlanks, (req, res, next) => {
+    // if the client attempts to change the `owner` property by including a new
+    // owner, prevent that by deleting that key/value pair
+    delete req.body.lesson.owner
+
+    Lesson.findById(req.params.id)
+        .then(handle404)
+        .then(lesson => {
+            // pass the `req` object and the Mongoose record to `requireOwnership`
+            // it will throw an error if the current user isn't the owner
+            requireOwnership(req, lesson)
+
+            // pass the result of Mongoose's `.update` to the next `.then`
+            return lesson.updateOne(req.body.lesson)
+        })
+        // if that succeeded, return 204 and no JSON
+        .then(() => res.sendStatus(204))
+        // if an error occurs, pass it to the handler
+        .catch(next)
+})
+
+//delete
+router.delete('/lessons/:id', requireToken, (req, res, next) => {
+    Lesson.findById(req.params.id)
+        .then(handle404)
+        .then(lesson => {
+            // throw an error if current user doesn't own `example`
+            requireOwnership(req, lesson)
+                // delete the example ONLY IF the above didn't throw
+            lesson.deleteOne()
+        })
+        // send back 204 and no content if the deletion succeeded
+        .then(() => res.sendStatus(204))
+        // if an error occurs, pass it to the handler
         .catch(next)
 })
 
